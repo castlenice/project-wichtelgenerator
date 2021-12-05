2d8e25ec289ed185912bff3774ff3fd42935fbc1
castlenice <sonja.schoenburg@gmail.com>
2021-12-05 20:10:46 +0100

    Wichtel-Generator verbessert

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 
 import './App.css';
+import { useState } from 'react';
+
+let liste_beschenkerin = ["Antje", "Thorsten", "Renate", "Rike", "Leif", "Ole", "Ben", "Sonja"];
+let liste_empfaenger = ["Antje", "Thorsten", "Renate", "Rike", "Leif", "Ole", "Ben", "Sonja"];
 
 function App() {
-  const names_1 = ["Antje", "Thorsten", "Renate", "Rike", "Ole", "Ben", "Sonja"];
-  const names_2 = ["Antje", "Thorsten", "Renate", "Rike", "Ole", "Ben", "Sonja"];
+  
+  const [names, setNames] = useState(["Mensch", "Mensch"]) 
+   
+  function randomName() {
+    if (liste_beschenkerin.length === 0 && liste_empfaenger.length === 0) {
+      return
+    }
 
-  const random_names_1 = names_1[Math.floor(Math.random() * names_1.length)];
-  const random_names_2 = names_2[Math.floor(Math.random() * names_2.length)];
+    const random_names_1 = liste_beschenkerin[Math.floor(Math.random() * liste_beschenkerin.length)];
+    let random_names_2 = liste_empfaenger[Math.floor(Math.random() * liste_empfaenger.length)];
+
+    while (random_names_2 === random_names_1) {
+        random_names_2 = liste_empfaenger[Math.floor(Math.random() * liste_empfaenger.length)]
+    }
+
+    liste_beschenkerin = liste_beschenkerin.filter(e => e !== random_names_1)
+    liste_empfaenger = liste_empfaenger.filter(e => e !== random_names_2)
+    setNames([random_names_1, random_names_2])
+  }
+ 
+  //arr = arr.filter(e => e !== 'B'); // will return ['A', 'C']
+   
 
   return (
     <div className="App">
       <header className="App-header">
-        <h2>{random_names_1 !== random_names_2 ? `${random_names_1} und ${random_names_2}`: "Try again"}</h2>
+        <h2 className="Names">{`${names[0]} beschenkt ${names[1]}`}</h2>
+        <button onClick={randomName}>Click to see names</button>
       </header>
     </div>
   );
